6d91ffe134eae504e5ed44bef08d1e7fcc0879c8
James Anderson <j.anderson2429@gmail.com>
2023-11-11 21:29:27 -0500

    Initial Commit

---
+++ PokeSync/PokeSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeSyncApp.swift
+//  PokeSync
+//
+//  Created by james Anderson on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PokeSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
